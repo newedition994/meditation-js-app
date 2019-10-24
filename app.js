b2b7daca14eb4762b205000defcd910c587e1096
Breyanna Vass <breyannavass@gmail.com>
2019-10-24 18:05:39 -0400

    finishing up some styles and selecting from the DOM

---
+++ app.js
@@ -0,0 +1,23 @@
+const app = () => {
+  const song = document.querySelector(".song");
+  const play = document.querySelector(".play");
+  const outline = document.querySelector(".moving-outline circle");
+  const video = document.querySelector(".vid-container video");
+
+  // Sounds
+  const sounds = document.querySelectorAll(".sound-picker button");
+
+  // Time Display
+  const tiemDisplay = document.querySelector(".time-display");
+
+  // Get length of the outline
+  const outlineLength = outline.getTotalLength();
+
+  // Duration
+  let fakeDuration = 600;
+
+  outline.style.strokeDasharray = outlineLength;
+  outline.style.strokeDashoffset = outlineLength;
+};
+
+app();
