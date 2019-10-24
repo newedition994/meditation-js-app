1076268242beeb5a80e824ff10af310a05dad956
Breyanna Vass <breyannavass@gmail.com>
2019-10-24 18:13:35 -0400

    getting the beach sounds and video to play as well as pause on click

--- app.js
+++ app.js
@@ -20,7 +20,22 @@ const app = () => {
   outline.style.strokeDashoffset = outlineLength;
 
   // play sound
-  play.addEventListener("click", () => {});
+  play.addEventListener("click", () => {
+    checkPlaying(song);
+  });
+
+  // function to pause and play the sound
+  const checkPlaying = song => {
+    if (song.paused) {
+      song.play();
+      video.play();
+      play.src = "./svg/pause.svg";
+    } else {
+      song.pause();
+      video.pause();
+      play.src = "./svg/play.svg";
+    }
+  };
 };
 
 app();
