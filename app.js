84d714ccb7cbafeb37b1653cee172eb1cdcf50b2
Breyanna Vass <breyannavass@gmail.com>
2019-10-24 18:35:45 -0400

    switch between the sounds and videos by picking a button

--- app.js
+++ app.js
@@ -20,6 +20,15 @@ const app = () => {
   outline.style.strokeDasharray = outlineLength;
   outline.style.strokeDashoffset = outlineLength;
 
+  // pick different sounds
+  sounds.forEach(sound => {
+    sound.addEventListener("click", function() {
+      song.src = this.getAttribute("data-sound");
+      video.src = this.getAttribute("data-video");
+      checkPlaying(song);
+    });
+  });
+
   // play sound
   play.addEventListener("click", () => {
     checkPlaying(song);
@@ -61,6 +70,13 @@ const app = () => {
 
     // Animate the time
     tiemDisplay.textContent = `${minutes}:${seconds}`;
+
+    if (currentTime >= fakeDuration) {
+      song.pause();
+      song.currentTime = 0;
+      play.src = "./svg/play.svg";
+      video.pause();
+    }
   };
 };
 
